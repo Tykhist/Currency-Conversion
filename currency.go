256498ec415ccb062380b0fe16ef5ed9d4b2d98e
William Smith <mr.williamjdsmith@gmail.com>
2022-06-07 03:01:44 -0700

    Update currency.go

--- currency.go
+++ currency.go
@@ -13,8 +13,30 @@ func main() {
     "OMR": 0.385,
   }
 
+  fmt.Println("Currencies & Rates:", currencies)
+  fmt.Println()
+
   var dollarAmount float32
   var currency string
 
+  fmt.Print("Enter dollar amount: ")
   fmt.Scan(&dollarAmount)
+  fmt.Println()
+
+  if dollarAmount == 0 {
+    fmt.Println("Error")
+  } else {
+    fmt.Print("Enter currency type: ")
+    fmt.Scan(&currency)
+    fmt.Println()
+
+    rate, isValid := currencies[currency]
+
+    if isValid == true {
+      fmt.Println("Result: ", rate*dollarAmount, currency)
+    } else {
+      fmt.Println("Currency not found...")
+    }
+
+  }
 }
