c85ecf2744485cde95d146e2017014dfbe537cf1
William Smith <mr.williamjdsmith@gmail.com>
2022-06-07 01:24:53 -0700

    Re-do currency.go
    Starting program over to determine source of bugs

--- currency.go
+++ currency.go
@@ -12,29 +12,9 @@ func main() {
     "KWD": 0.306,
     "OMR": 0.385,
   }
-  fmt.Println("Currencies & Rates:", currencies)
-  fmt.Println()
 
   var dollarAmount float32
   var currency string
-  fmt.Print("Enter a dollar amount above zero: ")
+
   fmt.Scan(&dollarAmount)
-  for { 
-    if dollarAmount == 0 {
-      fmt.Print("Error, enter a number: ")
-      fmt.Scan(&dollarAmount)
-    } else {
-      fmt.Print("Which currency would you like to convert to? ")
-      fmt.Scan(&currency)
-      rate, isValid := currencies[currency]
-      if isValid == true {
-        money := rate * dollarAmount
-        return fmt.Println("You now have", money)
-      } else {
-        fmt.Println("Error, currency not found. Enter dollar amount again: ")
-        fmt.Scan(&dollarAmount)
-      }
-      break
-    }
-  }
 }
